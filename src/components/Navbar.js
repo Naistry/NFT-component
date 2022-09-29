69f310d822f63e015e7d8eaca6cbbd444c2074d7
Martin <martinpastorino013@gmail.com>
2022-09-28 21:36:22 -0300

    arreglo de funciones

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import CardActive from './CardActive'
-import CardComing from './CardComing'
 import { NavLink, Outlet, Routes, Route } from 'react-router-dom'
 import './Navbar.css';
 
